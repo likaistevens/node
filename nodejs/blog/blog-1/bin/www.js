1328a595a73211cabec25d50b831ed31663daa0b
likai254 <likai254@jd.com>
2020-12-09 15:01:02 +0800

    2020-12-9

--- nodejs/blog/blog-1/bin/www.js
+++
@@ -1,8 +0,0 @@
-// www.js server技术相关的
-const http = require('http')
-
-const PORT = 8000
-const serverHandle = require('../app')
-
-const server = http.createServer(serverHandle)
-server.listen(PORT)
--- nodejs/blog/blog-1/readme.md
+++
@@ -1,5 +0,0 @@
-1.从0开始 不使用任何框架
-2.nodemon检测文件变化，自动重启node
-<!-- npm install --save-dev nodemon -->
-3.cross-env设置环境变量，兼容mac linux和windows
-<!-- npm install --save-dev cross-env -->
